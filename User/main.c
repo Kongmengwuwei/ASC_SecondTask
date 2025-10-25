1bd3a1430f67b8c58847b4ae999438665d6d385d
Kongmeng <kongmengwuwei@163.com>
2025-10-25 14:31:32 +0800

    Init project

---
+++ User/main.c
@@ -0,0 +1,26 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Key.h"
+#include "Timer.h"
+
+int main(void)
+{
+	OLED_Init();
+	Key_Init();
+	Timer_Init();
+	
+	while (1)
+	{
+		
+	}
+}
+
+void TIM2_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	{
+		Key_Tick();
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+	}
+}
